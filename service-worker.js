4aeea7d7ec9864d1a92bcef544cf4a27af5faf51
aguess2018 <a.guess2018@gmail.com>
2026-09-22 18:45:59 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "before-the-close-v140";
+const CACHE_NAME = "before-the-close-v141";
 const APP_FILES = ["./","./index.html","./style.css","./prayers.js","./app.js","./cloud.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
 
 self.addEventListener("install", event => {
